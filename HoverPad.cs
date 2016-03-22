9445c1a5c62cab69b8bb4e7e979797804de1031b
MilesIwes <miles.iwes@gmail.com>
2016-03-22 15:22:14 +0000

    Update HoverPad.cs
    Hover pad when inside trigger

--- HoverPad.cs
+++ HoverPad.cs
@@ -3,3 +3,18 @@ using System.Collections;
 
 public class HoverPad : MonoBehaviour
 {
+  void onTriggerEnter (Collider other)
+  {
+    Debug.log ("Do when inside")
+  }
+  
+  void onTriggerStay (Collider other)
+  {
+    Debug.log ("Do when stay inside")
+  }
+
+void onTriggerExit (Collider other)
+  {
+    Debug.log ("Do when exit")
+  }
+}
